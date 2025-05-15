5671a99563a3b5ad4b6f3fe00da69b4c9c5947b8
Phong <hoangdiep16041975@gmail.com>
2025-05-15 22:14:35 +0700

    cap nhat giao dien + hoan thien logic

--- Lab5-ConvertYears/app/src/main/java/com/example/convertyears/MainActivity.java
+++ Lab5-ConvertYears/app/src/main/java/com/example/convertyears/MainActivity.java
@@ -1,6 +1,9 @@
 package com.example.convertyears;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -15,10 +18,35 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.TableLayout1), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars( ));
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
+        });
+
+        // Ánh xạ view
+        EditText editNam = findViewById(R.id.editnamduonglich);
+        TextView txtKetQua = findViewById(R.id.textView5);
+        Button btnDoi = findViewById(R.id.button1);
+
+        // Xử lý nút chuyển đổi
+        btnDoi.setOnClickListener(v -> {
+            String namStr = editNam.getText().toString().trim();
+            if (!namStr.isEmpty()) {
+                int namDuong = Integer.parseInt(namStr);
+
+                String[] canList = {"Canh", "Tân", "Nhâm", "Quý", "Giáp", "Ất", "Bính", "Đinh", "Mậu", "Kỷ"};
+                String[] chiList = {"Thân", "Dậu", "Tuất", "Hợi", "Tý", "Sửu", "Dần", "Mão", "Thìn", "Tỵ", "Ngọ", "Mùi"};
+
+                String can = canList[namDuong % 10];
+                String chi = chiList[namDuong % 12];
+
+                String namAmLich = can + " " + chi;
+                txtKetQua.setText(namAmLich);
+            } else {
+                txtKetQua.setText("Vui lòng nhập năm!");
+            }
         });
     }
 }
